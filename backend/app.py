2b41e8591336d0dc7b6ee5c84d75dc1f0bcd7270
Jude Adham <judeadham@gmail.com>
2022-01-22 11:37:57 -0500

    Added POST /session/<session_id>/address

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,6 @@
+import requests
+import os
+from urllib.parse import quote
 from dotenv import load_dotenv
 from flask import Flask, request
 import firebase_admin
@@ -33,6 +36,39 @@ def createSession():
     })
 
     return {'session_id': session_ref.id}, 201
+
+
+@app.route('/session/<session_id>/address', methods=['POST'])
+def addresss(session_id):
+    if not db.collection(u'sessions').document(session_id).get().exists:
+        return {
+            'message': "Error! Invalid session ID."
+        }, 400
+    if 'address' not in request.form:
+        return {
+            'message': "Error! 'address' not found in request."
+        }, 400
+
+    url = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json'
+    params = {
+        'input': request.form['address'],
+        'inputtype': 'textquery',
+        'fields': 'formatted_address',
+        'key': os.environ.get('GCLOUD_MAPS_API_KEY')
+        }
+
+    response = requests.get(url, params=params).json()
+    if response['status'] != 'OK':
+        return {
+            'message': "Error! Invalid address."
+        }, 400
+
+    fmt_address = response['candidates'][0]['formatted_address']
+
+    session_ref = db.collection(u'sessions').document(session_id)
+    session_ref.update({u'user_address': fmt_address})
+
+    return {'address': fmt_address}, 200
 
 
 if __name__ == "__main__":
